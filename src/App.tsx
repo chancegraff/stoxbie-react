8644b1f2d48ad3030ead3a47807915557231f0f3
Chance Graff <cgraffrun@gmail.com>
2020-07-24 19:04:14 -0500

    Add typescript, scss support

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
