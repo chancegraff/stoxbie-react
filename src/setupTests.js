c5cdc83c4547bed6417669fab1e83bb760c54835
Chance Graff <cgraffrun@gmail.com>
2020-08-10 15:29:20 -0500

    Create test readme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
-import ResizeObserver from "tests/mocks/ResizeObserver";
-
 import "@testing-library/jest-dom/extend-expect";
+
+import ResizeObserver from "tests/mocks/ResizeObserver";
 
 global.document.createRange = () =>
 {
@@ -23,29 +23,3 @@ global.document.createRange = () =>
 delete window.location;
 
 window.location = { reload: jest.fn() };
-
-// Mocking dependencies (in case we forget again)
-// const getFirstAlbumTitle = require('./index');
-// const axios = require('axios');
-
-// jest.mock('axios');
-
-// it('returns the title of the first album', async () => {
-//   axios.get.mockResolvedValue({
-//     data: [
-//       {
-//         userId: 1,
-//         id: 1,
-//         title: 'My First Album'
-//       },
-//       {
-//         userId: 1,
-//         id: 2,
-//         title: 'Album: The Sequel'
-//       }
-//     ]
-//   });
-
-//   const title = await getFirstAlbumTitle();
-//   expect(title).toEqual('My First Album');
-// });
