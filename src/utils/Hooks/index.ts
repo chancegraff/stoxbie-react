a09b0980e2e4ebc816a83f602d5bab67605cc7d6
chancegraff <cgraffrun@gmail.com>
2020-09-02 22:06:14 -0500

    Fix hooks exports

--- src/utils/Hooks/index.ts
+++ src/utils/Hooks/index.ts
@@ -1,2 +1,11 @@
-export * from "./useHover";
-export * from "./useScrollToTop";
+export {
+  HoverState,
+  useHover,
+} from "./useHover";
+export type {
+  HandleMouseEnter,
+  HandleMouseLeave,
+} from "./useHover";
+export {
+  useScrollToTop,
+} from "./useScrollToTop";
